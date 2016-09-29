bd49057cff3a737151fb615b4e6c3c83dedece45
AminaLatief <AminaLatief12@gmail.com>
2016-09-29 15:10:37 +0200

    DSC

---
+++ Application/src/DSC/LoadIcon.java
@@ -0,0 +1,23 @@
+                                       /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DSC;
+
+import java.net.URL;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author Amina
+ */
+public class LoadIcon {
+    public void iconLoader(JLabel label2){
+        URL url = this.getClass().getResource("load_2.gif");
+        Icon icon2 = new ImageIcon(url);
+        label2.setIcon(icon2);label2.setOpaque(false);
+    }
+}
